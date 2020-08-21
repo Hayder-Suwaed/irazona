72e92feba8738594673d368c262db5574a3727e9
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-08-20 22:03:37 -0600

    added styling of products page

--- frontend/src/Screens/homeScreen.js
+++ frontend/src/Screens/homeScreen.js
@@ -1,35 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import data from "../data";
+import React from 'react';
+import { Link } from 'react-router-dom'
+import data from '../data';
 
 function HomeScreen(props) {
-  return (
-    <ul className="products">
-      {data.products.map((product) => (
+  return <ul className="products">
+    {
+      data.products.map(product =>
         <li>
           <div className="product">
-            <Link to={"/products/" + product._id}>
-              {" "}
-              <img
-                className="product-image"
-                src={product.image}
-                alt="product"
-              />
-            </Link>
+            <Link to={'/product/' + product._id}>
+              <img className="product-image" src={product.image} alt="product" />
 
+            </Link>
             <div className="product-name">
-              <Link to={"/products/" + product._id}>{product.name}</Link>
+              <Link to={'/product/' + product._id}>{product.name}</Link>
             </div>
             <div className="product-brand">{product.brand}</div>
-            <div className="product-price">{product.price}</div>
-            <div className="product-rating">
-              {product.rating} Stars {product.numReviews}
-            </div>
+            <div className="product-price">${product.price}</div>
+            <div className="product-rating">{product.rating} Stars ({product.numReiews} Reviews)</div>
           </div>
-        </li>
-      ))}
-    </ul>
-  );
-}
+        </li>)
+    }
 
-export default HomeScreen;
+
+
+  </ul>
+}
+export default HomeScreen; 
