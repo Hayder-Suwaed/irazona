f3c21cb92d377649052d40c3c0aa3d37ddc007e8
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-08-20 23:20:34 -0600

    fix indentation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,14 +14,12 @@ function App() {
   };
 
   return (
-<BrowserRouter>
+    <BrowserRouter>
       <div className="grid-container">
         <header className="header">
           <div className="brand">
-            <button onClick={openMenu}>
-              &#9776;
-        </button>
-            <Link to="/" >amazona</Link>
+            <button onClick={openMenu}>&#9776;</button>
+            <Link to="/">amazona</Link>
           </div>
           <div className="header-links">
             <a href="cart.html">Cart</a>
@@ -30,7 +28,9 @@ function App() {
         </header>
         <aside className="sidebar">
           <h3>Shopping Categories</h3>
-          <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+          <button className="sidebar-close-button" onClick={closeMenu}>
+            x
+          </button>
           <ul>
             <li>
               <a href="index.html">Pants</a>
@@ -39,22 +39,16 @@ function App() {
             <li>
               <a href="index.html">Shirts</a>
             </li>
-
           </ul>
         </aside>
         <main className="main">
           <div className="content">
             <Route path="/product/:id" component={ProductScreen} />
             <Route path="/" exact={true} component={HomeScreen} />
-
-
           </div>
-
         </main>
-        <footer className="footer">
-          All right reserved.
-    </footer>
-    </div>
+        <footer className="footer">All right reserved.</footer>
+      </div>
     </BrowserRouter>
   );
 }
