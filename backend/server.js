9318ef9bc492becc8e289d0f49c8751f8fb39ca6
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-08-20 23:16:37 -0600

    create server with node

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import express from "rxpress";
+import data from "./data";
+const app = express();
+
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+
+app.listen(5000, () => console.log("Server started at http://localhost:5000"));
