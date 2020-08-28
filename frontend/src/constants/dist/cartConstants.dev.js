8f4f22857085b442440feede62f90254ebf0f2bf
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-08-27 23:17:39 -0600

    create Add to Cart page

--- frontend/src/constants/dist/cartConstants.dev.js
+++ frontend/src/constants/dist/cartConstants.dev.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.CART_REMOVE_ITEM = exports.CART_ADD_ITEM = void 0;
-var CART_ADD_ITEM = 'CART_ADD_ITEM';
+var CART_ADD_ITEM = "CART_ADD_ITEM";
 exports.CART_ADD_ITEM = CART_ADD_ITEM;
-var CART_REMOVE_ITEM = 'CART_REMOVE_ITEM';
+var CART_REMOVE_ITEM = "CART_REMOVE_ITEM";
 exports.CART_REMOVE_ITEM = CART_REMOVE_ITEM;
