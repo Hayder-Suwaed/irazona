18a83681618d8da85b4c3044a1d70253429b6e94
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-08-29 21:56:17 -0600

    Create Schema

--- backend/models/dist/userModel.dev.js
+++ backend/models/dist/userModel.dev.js
@@ -1,5 +1,10 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
 var _mongoose = _interopRequireDefault(require("mongoose"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
@@ -24,3 +29,8 @@ var userSchema = new _mongoose["default"].Schema({
     "default": false
   }
 });
+
+var userModel = _mongoose["default"].model("User", userSchema);
+
+var _default = userModel;
+exports["default"] = _default;
